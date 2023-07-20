b3d2f1314e9e5323ced395e22c01d09a83ee0b6a
muzamilconvo <139231269+muzamilconvo@users.noreply.github.com>
2023-07-20 11:01:42 +0500

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,165 @@
+//Question 1
+// Write a program that checks if a given number is positive, negative, or zero. Display the
+//corresponding message using if-else statements.
+
+// var num = 9;
+// if (num < 0) {
+//   console.log("Number is negative");
+// } else if (num > 0) {
+//   console.log("Number is positive");
+// } else {
+//   console.log("Number is zero");
+// }
+
+//Question 2
+//Create a program that determines if a given number is a leap year or not. Display the
+//appropriate message using if-else statements. (A leap year is divisible by 4, but not by
+//    100, unless it is divisible by 400.)
+
+// var year = 2021;
+// if (year % 4 === 0) {
+//   if (year % 100 === 0) {
+//     if (year % 4 === 0) {
+//       console.log("The Year " + year + " is a leap year");
+//     } else {
+//       console.log("The Year " + year + " is  not a leap year");
+//     }
+//   } else {
+//     console.log("The Year " + year + " is   a leap year");
+//   }
+// } else {
+//   console.log("The Year " + year + " is a  not leap year");
+// }
+
+//Question 3
+//Create a program that calculates the grade based on a given score. Display the
+// corresponding grade using if-else statements according to the following scale:
+// 90 or above: A
+// 80 to 89: B
+// 70 to 79: C
+// 60 to 69: D
+// Below 60: F
+
+// var tot_marks = -1;
+// if (tot_marks < 100 && tot_marks > -1) {
+//   if (tot_marks >= 90) {
+//     console.log("Grade A");
+//   } else if (tot_marks >= 80) {
+//     console.log("Grade B");
+//   } else if (tot_marks >= 70) {
+//     console.log("Grade C");
+//   } else if (tot_marks >= 60) {
+//     console.log("Grade D");
+//   } else {
+//     console.log("Grade F");
+//   }
+// } else {
+//   console.log("Please enter Valid marks");
+// }
+
+//Question 4
+//Create a program that checks the day of the week based on a given number (1 for
+//Monday, 2 for Tuesday, etc.). Display the corresponding day using a switch statement.
+
+// var num = 2;
+// switch (num) {
+//   case 1: {
+//     console.log("Monday");
+//     break;
+//   }
+//   case 2: {
+//     console.log("Tuesday");
+//     break;
+//   }
+//   case 3: {
+//     console.log("Wednesday");
+//     break;
+//   }
+//   case 4: {
+//     console.log("Thursday");
+//     break;
+//   }
+//   case 5: {
+//     console.log("Friday");
+//     break;
+//   }
+//   case 6: {
+//     console.log("Saturday");
+//     break;
+//   }
+//   case 7: {
+//     console.log("Sunday");
+//     break;
+//   }
+//   default: {
+//     console.log("Enter number between 1 and 7");
+//     break;
+//   }
+// }
+
+//Question 5
+// Write a program that checks the type of a given value and performs specific actions
+// based on the type using a switch statement. For example, if the value is a number,
+// multiply it by 2; if it is a string, concatenate "Hello " before it; if it is a boolean, negate it.
+
+// var value = false;
+// var check = typeof value;
+// console.log(check);
+// switch (check) {
+//   case "number": {
+//     console.log("its number so :" + value * 2);
+//     break;
+//   }
+//   case "boolean": {
+//     console.log("its number so :" + !value);
+//     break;
+//   }
+//   case "string": {
+//     console.log("its string so " + (value + " " + "hello"));
+//     break;
+//   }
+//   default: {
+//     console.log("Enter valid Data");
+//     break;
+//   }
+// }
+
+// Question 6
+// Create a program that checks the day of the week based on a given number (1 for
+// Monday, 2 for Tuesday, etc.). Display the corresponding day using a switch statement.
+
+// var num = 2;
+// switch (num) {
+//   case 1: {
+//     console.log("Monday");
+//     break;
+//   }
+//   case 2: {
+//     console.log("Tuesday");
+//     break;
+//   }
+//   case 3: {
+//     console.log("Wednesday");
+//     break;
+//   }
+//   case 4: {
+//     console.log("Thursday");
+//     break;
+//   }
+//   case 5: {
+//     console.log("Friday");
+//     break;
+//   }
+//   case 6: {
+//     console.log("Saturday");
+//     break;
+//   }
+//   case 7: {
+//     console.log("Sunday");
+//     break;
+//   }
+//   default: {
+//     console.log("Enter number between 1 and 7");
+//     break;
+//   }
+// }
